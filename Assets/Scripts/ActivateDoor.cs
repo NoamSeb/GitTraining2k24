b7a41fcb45dc73a8fdc62fbbd9bfd2d79ea233e6
Margot <margotdelage02@gmail.com>
2024-10-17 10:52:14 +0200

    [FEAT] learderboard + request api

--- Assets/Scripts/ActivateDoor.cs
+++ Assets/Scripts/ActivateDoor.cs
@@ -8,11 +8,14 @@ public class ActivateDoor : MonoBehaviour
 
     bool CanMove = false;
 
+    [SerializeField] Timer timer;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player") && collision.gameObject.GetComponent<Key>().CanActivateDoor)
         {
             CanMove = true;
+            timer.finJeu = true;
             //play animation ouverture ?
         }
     }
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -7,9 +7,13 @@ using static Unity.Burst.Intrinsics.X86.Avx;
 
 public class Timer : MonoBehaviour
 {
-    float timer = 0;
+    public float timer = 0;
 
     TextMeshProUGUI tmp;
+
+    public bool finJeu = false;
+
+    [SerializeField] LeaderBoard lb;
 
     private void Start()
     {
@@ -19,8 +23,15 @@ public class Timer : MonoBehaviour
 
     void Update()
     {
-        timer += Time.deltaTime;
-        var ts = TimeSpan.FromSeconds(timer);
-        tmp.text = string.Format("{0:00}:{1:00}", ts.Minutes, ts.Seconds);
+        if (!finJeu)
+        {
+            timer += Time.deltaTime;
+            var ts = TimeSpan.FromSeconds(timer);
+            tmp.text = string.Format("{0:00}:{1:00}", ts.Minutes, ts.Seconds);
+        } else if (lb != null)
+        {
+            lb.timerInt = (int)timer;
+            lb.timerString = tmp.text;
+        }
     }
 }
